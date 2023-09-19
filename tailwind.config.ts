0caac08c19300679e60bf0fb8ea7a1a9fa0c54ee
adelnizamutdinov <stiggpwnz@gmail.com>
2023-09-19 16:51:17 +0800

    amen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import { type Config } from "tailwindcss"
 
 // noinspection JSUnusedGlobalSymbols
 export default {
-  content: ["src/**/*.{js,jsx,ts,tsx}"],
+  content: ["src/**/*.{js,jsx,ts,tsx}","index.html"],
   theme: {
     extend: {},
   },
